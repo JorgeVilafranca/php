dd7576093c8234ea7e42869ca9e64676430f97f8
Jorge <jjdrako@yopmail.com>
2018-03-25 20:08:01 +0200

    principios

---
+++ documents/funcion_texto.php
@@ -0,0 +1,8 @@
+<?php
+
+    // funciones
+    function dameDatos() {
+        echo "<p>Este es el mensaje de la función</p>";
+    }
+
+?>
---
+++ documents/principios.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primera pagina PHP</title>
+</head>
+<body>
+<?php
+    // Escribimos un texto con print
+    print "<p>Bienvenidos al curso de PHP <br>";
+    print "Hola a todos <br>";
+    print "Hasta la próxima</p>";
+
+    $nombre = "Jorge";
+    $edad = 35;
+
+    // Se concatena con . no es igual poner una cadena de texto con comillas dobles que con comillas simples
+    print "<p>El nombre de usuario es $nombre</p>";
+    print '<p>El nombre de usuario es $nombre</p>';
+
+    /*
+        print es una funcion y echo una expresion, por lo que para que no evalue se usa mas echo para imprimir por pantalla,
+        ademas echo deja pasar las variables como parametros
+    */
+    echo $nombre,$edad;
+
+    /*
+        con include si el archivo falta dara error pero seguira ejecutando el codigo
+        require ademas del error no ejecutara mas codigo
+    */
+    include ("./funcion_texto.php");
+    
+    dameDatos();
+?>
+</body>
+</html>
